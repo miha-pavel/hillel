eb7d2f6dcdb74e9f58753ee1b755716ff84ea0ad
miha-pavel <pahamihali4@gmail.com>
2019-12-13 11:54:04 +0200

    master -- avefage params

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from flask import Flask
 # from random import string
 from faker import Faker
 from livereload import Server
+import pandas as pd
 
 # app is a Flask object
 app = Flask('app')
@@ -20,18 +21,11 @@ def home_page():
     return 'This Site is hometask '
 
 
-# @app.route('/gen')
-# def gen():
-#     return ''.join(
-#         random.choice(string.ascii_uppercase) for i in range(10)
-#     )
-
-
+style_string = "style='border: 1px solid black;'"
 @app.route('/user_data')
 def user_data():
-    style_string = "style='border: 1px solid black;'"
     users_table = """<h1>List 100 random Users</h1>
-    <table style="border: 1px solid black;">
+    <table {style}>
     <tr>
         <th {style}>User Id</th>
         <th {style}>User name</th>
@@ -54,6 +48,42 @@ def user_data():
                                     )
     return users_table.format(users_data=users_data,
                             style=style_string)
+
+
+@app.route('/average_params')
+def average_params():
+    df = pd.read_csv('hw.csv', sep=',')
+    header_list = df.columns.tolist()
+    header_list.pop(0)
+    height = "Height"
+    weight = "Weight"
+    average_param = ""
+    average_table = """
+                        <h1>Average Data</h1>
+                        <table {style}>
+                            {average_param}
+                        </table>
+                    """
+    for column_name in header_list:
+        if height in column_name:
+            average_param = average_param + """
+                <tr>
+                    <td {style}>{height} (Cm)</td>
+                    <td {style}>{height_data}</td>
+                </tr>
+            """.format(height=height,
+            height_data=round(df[column_name].mean()*2.54, 2),
+            style=style_string)
+        else:
+            average_param = average_param + """
+                <tr>
+                    <td {style}>{weight} (Kg)</td>
+                    <td {style}>{weight_data}</td>
+                </tr>
+            """.format(weight=weight,
+            weight_data=round(df[column_name].mean()*0.453592, 2),
+            style=style_string)
+    return average_table.format(average_param=average_param, style=style_string)
 
 
 if __name__ == "__main__":
